14c8f3f1f6e754c37aeb792a0bd040e912ae1a9f
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-11-05 16:20:25 +0000

    Refactor codebase

--- profile.py
+++ profile.py
@@ -1,19 +1,20 @@
 import os
 
+
 def current_user_directory():
-    """ This function gets the user current working directory"""
+    """This function gets the user current working directory"""
     current_directory = os.getcwd()
     username = os.path.basename(current_directory)
     return current_directory, username
 
 
 def get_profile(user_directory):
-    """ This function gets the path to the profile.txt file."""
+    """This function gets the path to the profile.txt file."""
     return os.path.join(user_directory, ".profile.txt")
 
 
 def read_profile(profile_path):
-    """ Read and display the profile of data if it exists."""
+    """Read and display the profile of data if it exists."""
     with open(profile_path, "r") as file:
         content = file.read().strip()
         print("Recent profile data")
@@ -26,6 +27,6 @@ def update_profile(profile_path):
     name = input("Enter name: ")
     phone_number = input("Enter phone number: ")
     email = input("Enter email address: ")
-    with open(profile_path, 'w') as file:
+    with open(profile_path, "w") as file:
         file.write(f"Name: {name}\nPhone: {phone_number}\nEmail: {email}")
-    print("Profile updated successfully.")    
+    print("Profile updated successfully.")
